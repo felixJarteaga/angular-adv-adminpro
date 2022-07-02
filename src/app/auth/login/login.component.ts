2d406f6919e0bea777355919da41410df798c677
Felix Arteaga <farteaga21791@gmail.com>
2022-07-02 14:57:10 +0200

    Login y Auth

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,19 +1,77 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, AfterViewInit, ViewChild, ElementRef, NgZone } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { UsuarioService } from 'src/app/services/usuario.service';
+import Swal from 'sweetalert2';
+
+declare const google:any;
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: [ './login.component.css' ]
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent implements AfterViewInit  {
 
-  constructor( private router:Router ) { }
+  @ViewChild('googleBtn') googleBtn:ElementRef;
 
-  ngOnInit(): void {
+  formSubmitted = false;
+
+  loginForm = this.fb.group({
+    email: [localStorage.getItem('email') || '', [Validators.required, Validators.email]],
+    password: ['', Validators.required],
+    remember:[false]
+  });
+
+  constructor( private router:Router, private fb:FormBuilder, private usuarioService:UsuarioService, private ngZone:NgZone ) { }
+
+  ngAfterViewInit(): void {
+    this.googleInit();
   }
 
+  googleInit(){
+      google.accounts.id.initialize({
+        client_id: '919867093196-s9jge0rq3feu0lu605aaga1vqbm2l8sc.apps.googleusercontent.com',
+        callback:  (response:any) => this.handleCredentialResponse(response)
+      });
+      google.accounts.id.renderButton(
+        // document.getElementById("buttonDiv"),
+        this.googleBtn.nativeElement,
+        { theme: "outline", size: "large" }  // customization attributes
+      );
+  }
+
+handleCredentialResponse(response:any){
+  // console.log("Encoded JWT ID token: " + response.credential);
+  this.usuarioService.loginGoogle( response.credential ).subscribe(
+    ( resp ) => {
+
+      this.ngZone.run( () => {
+        this.router.navigateByUrl('/');
+      })
+    }
+  );
+}
+
   login(){
-    this.router.navigateByUrl('/');
+    
+    this.usuarioService.login( this.loginForm.value ).subscribe(
+      ( resp ) => {
+        if ( this.loginForm.get('remember').value ) {
+          localStorage.setItem('email', this.loginForm.get('email').value);
+        }else{
+          localStorage.removeItem('email');
+        }
+
+        this.router.navigateByUrl('/');
+      },
+      (err) =>{
+        // Si sucede un error
+        Swal.fire('Error',err.error.msg, 'error');
+      }
+    );
+    // console.log( this.loginForm.value );
+    
+    // this.router.navigateByUrl('/');
   }
 }
---
+++ src/app/services/usuario.service.ts
@@ -0,0 +1,80 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable, NgZone } from '@angular/core';
+import { catchError, map, tap } from 'rxjs/operators';
+import { Observable, of } from 'rxjs';
+import { Router } from '@angular/router';
+
+import { environment } from 'src/environments/environment';
+
+import { LoginForm } from '../interfaces/login-form.interface';
+import { RegisterForm } from '../interfaces/register-form.interface';
+
+declare const google:any;
+
+const base_url = environment.base_url;
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsuarioService {
+
+  constructor( private http:HttpClient, private router:Router, private ngZone:NgZone ) { }
+
+  logout(){
+    localStorage.removeItem('token');
+    
+    google.accounts.id.revoke( 'farteaga21791@gmail.com', () => {
+
+      this.ngZone.run( () => {
+        this.router.navigateByUrl('/login');
+      })
+    });
+  }
+
+  validarToken():Observable<boolean>{
+    const token = localStorage.getItem('token') || '';
+    return this.http.get(`${ base_url }/login/renew`,{
+      headers: {
+        'x-token': token
+      }
+    }).pipe(
+      tap( (resp:any) =>{
+        localStorage.setItem('token',resp.token);
+      }),
+      map( resp => {
+        return true;
+      }),
+      catchError( error => of(false))
+    );
+  }
+
+  crearUsuario( formData:RegisterForm ){
+    console.log('creando usuario');
+    return this.http.post(`${base_url}/usuarios`,formData)
+                    .pipe(
+                      tap( (resp:any) => {
+                        localStorage.setItem('token',resp.token);
+                      })
+                    );
+  }
+
+  login( formData:LoginForm ){
+    return this.http.post(`${base_url}/login`,formData)
+                    .pipe(
+                      tap( (resp:any) => {
+                        localStorage.setItem('token',resp.token);
+                      })
+                    );
+  }
+
+  loginGoogle(token:string){
+    return this.http.post(`${ base_url }/login/google`,{ token })
+                  .pipe(
+                   tap( (resp:any) => {
+                        // console.log(resp);
+                        localStorage.setItem('token',resp.token);
+                      })
+                  )
+  }
+
+}
